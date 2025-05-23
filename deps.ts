1834af5172914d4807afb3fe43baa8ff1b77e880
Шомуродов <cometa11we@gmail.com>
2025-05-23 11:34:39 -0700

    add: axiod

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
 export * from "https://deno.land/x/grammy@v1.21.1/mod.ts";
 import "https://deno.land/std@0.201.0/dotenv/load.ts";
+export * from "https://deno.land/x/axiod/mod.ts"
 
 export const env = Deno.env
